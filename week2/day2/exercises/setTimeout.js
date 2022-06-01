a7de925936751d5507762a1a970d35dcb97fe3f6
Emir Furkan Arslan <95889438+EmirArslann@users.noreply.github.com>
2022-06-01 15:01:19 -0700

    a

--- week2/day2/exercises/setTimeout.js
+++ week2/day2/exercises/setTimeout.js
@@ -11,6 +11,11 @@ All of the words can appear at the same time.
 
 //Code here...
 
+// setTimeout(()=>{
+//     process.stdout.write("4311o th3r3 w0r1d")
+
+// }, 500)
+
 /*
 Challenge #2
 Next, let's make things more interesting...
@@ -20,5 +25,22 @@ Instead of printing the entire sentence together, have our code print out each o
 
 It's okay if each of the words appears on a separate line, due to console.log.
 */
-
+const str = "4311o th3r3 w0r1d"
+let i = 0
 //Code here...
+setTimeout(()=>{
+    let arr = str.split(" ")
+
+    for(let i in arr){
+        setTimeout(() => {
+           process.stdout.write(arr[i] + " ") 
+        }, 1000 * i);
+        
+        
+    }
+
+    
+    
+    
+    
+}, 1000)
