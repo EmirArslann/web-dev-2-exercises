f357e9ac368d0e1f09017ae5432d2c126c0af9ef
Emir Furkan Arslan <95889438+EmirArslann@users.noreply.github.com>
2022-06-15 16:29:29 -0700

    cornercoin

--- week4/day3/cornercoin/index.js
+++ week4/day3/cornercoin/index.js
@@ -1,24 +1,55 @@
-let balance = 500.0;
+
+
+class Account {
+  constructor(username){
+    this.username = username;
+    this.balance = 0
+  }
+
+  
+}
 
 class Withdrawal {
-  constructor(amount) {
+  constructor(account,amount) {
     this.amount = amount;
+    this.account = account
   }
 
   commit() {
-    balance -= this.amount;
+    this.account.balance -= this.amount;
   }
+
+
+}
+
+class Deposit {
+  constructor(account , amount){
+    this.amount = amount
+    this.account = account
+  }
+  
+  commit() {
+    this.account.balance += this.amount
+  }
+
 }
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account('snow-patrol');
+console.log(myAccount);
+t1 = new Withdrawal(myAccount,50.25);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(myAccount,9.99);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+t3 = new Deposit(myAccount,120.0);
+t3.commit();
+console.log('Transaction 3:', t3);
+
+
+
