923d4abcccbef8093ddbc1e7e5d3a077a57c1409
Emir Furkan Arslan <95889438+EmirArslann@users.noreply.github.com>
2022-06-22 16:55:36 -0700

    exercise

--- week5/day3/exercise/index.js
+++ week5/day3/exercise/index.js
@@ -1,3 +1,53 @@
-$(() => {
-  // code here
+$(document).ready(() => {
+  $.ajax({
+    method: 'get',
+    url: 'https://jsonplaceholder.typicode.com/posts',
+    dataType: 'json',
+  }).done((data) => {
+    console.log(data)
+  $.each(data, (i, post) => {
+     $('.posts').append(`
+        <div class="post" id=${post.id}>
+        <h2>${post.title}</h2>
+        <p>${post.body}</p>
+        <button id="tog">Comments</button>
+       
+      </div>
+      `); 
+      $(`#${post.id}`).on('click', () =>{
+        $('.comms').fadeToggle(1000, () => {
+          console.log("toggled")
+        });
+      });
+    });
+  });
+  $.ajax({
+    method: 'get',
+    url: 'https://jsonplaceholder.typicode.com/comments',
+    dataType: 'json'
+  }).done((data) => {
+    console.log(data, 'data2')
+    $.each(data, (i, comment) =>{
+      $(`#${comment.postId}`).append(`
+       <div class="comms" id=${comment.postId}>
+           <p>${comment.name}</p>
+           <p>${comment.email}</p>
+           <p>${comment.body}</p>
+        </div>
+
+          
+
+
+      `)
+    })
+
+  })
+  
+
+  
+
 });
+
+
+
+
